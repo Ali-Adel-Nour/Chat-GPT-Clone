f8b307e077026fea838be261fc37592df197f2aa
Ali-Adel-Nour <aliiiadel841@gmail.com>
2023-05-16 21:10:38 +0300

    making the interface using react

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <section className="side-bar">
+        <button>New Chat</button>
+        <ul className="history"></ul>
+        <nav>
+          <p>Made by Ali</p>
+        </nav>
+      </section>
+      <section className="main"></section>
     </div>
   );
 }
