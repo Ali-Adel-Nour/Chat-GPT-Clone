399f51aca8d9b39cf4e955a05c7bd6b86125aeda
Ali-Adel-Nour <aliiiadel841@gmail.com>
2023-05-18 02:47:17 +0300

    making the interface

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,27 @@ function App() {
     <div className="app">
       <section className="side-bar">
         <button>New Chat</button>
-        <ul className="history"></ul>
+        <ul className="history">
+          <li></li>
+        </ul>
         <nav>
           <p>Made by Ali</p>
         </nav>
       </section>
-      <section className="main"></section>
+      <section className="main">
+        <h1>AliGPT</h1>
+        <ul className="feed"></ul>
+        <div className="bottom-section">
+          <div className="input-container">
+
+            <input />
+            <div id="submit">✅</div>
+          </div>
+        </div>
+        <p className="info">
+          Free Research Preview. ChatGPT may produce inaccurate information about people, places, or facts. ChatGPT May 12 Version
+        </p>
+      </section>
     </div>
   );
 }
